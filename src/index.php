8e5a7dba29df04fa85f4709db43189f9366e1ce0
Macc Liu <maccliu@qq.com>
2017-03-04 14:05:09 +0800

    开发中

--- src/index.php
+++ src/index.php
@@ -3,6 +3,16 @@
  * Dida Framework --Powered by Zeupin LLC
  * http://dida.zeupin.com
  */
+/* 开始计时 */
+define('DIDA_START_TIME', microtime(true));
+
+/* 检测运行环境 */
+define('IS_CLI', PHP_SAPI === 'cli');
+
+/* 导入通用函数库 */
+require DIDA_ROOT . 'Functions/Dida.php';
+
+/* 加载autoload机制 */
 require __DIR__ . '/autoload.php';
 
 /* 生成app实例 */
