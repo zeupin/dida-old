da36dfb69bf9611311bb06ad9e4758faaf4cabae
Macc Liu <maccliu@qq.com>
2017-03-05 22:06:48 +0800

    WWW_ROOT

--- src/Constants.php
+++ src/Constants.php
@@ -6,6 +6,7 @@
 define('IS_CLI', PHP_SAPI === 'cli');
 
 if (!IS_CLI) {
+    /* REQUEST_METHOD */
     if (isset($_POST['_method'])) {
         define('REQUEST_METHOD', strtoupper($_POST['_method']));
     } elseif (isset($_SERVER['REQUEST_METHOD'])) {
@@ -14,5 +15,9 @@ if (!IS_CLI) {
         define('REQUEST_METHOD', '');
     }
 
+    /* WWW_ROOT */
+    define('WWW_ROOT', str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME'])));
+
+    /* IS_AJAX */
     define('IS_AJAX', isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest');
 }
