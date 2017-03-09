6691350dac8b3bcb7b80315a6a939e2e76936005
Macc Liu <maccliu@qq.com>
2017-03-09 09:58:57 +0800

    Response

--- src/Response/Response.php
+++ src/Response/Response.php
@@ -11,4 +11,62 @@ namespace Dida;
  */
 class Response
 {
+    protected $head = [];
+    protected $body = [];
+    protected $format = 'html';
+    protected $encoding = 'utf-8';
+
+
+    public function output()
+    {
+    }
+
+
+    public function sendHeader()
+    {
+    }
+
+
+    public function redirect($url)
+    {
+        header("Location: $url");
+    }
+
+
+    public function addHead($head)
+    {
+        $this->head[] = $head;
+    }
+
+
+    public function addBody($body)
+    {
+        $this->body[] = $body;
+    }
+
+
+    public function setFormat($format)
+    {
+        $this->format = $format;
+        return $this;
+    }
+
+
+    public function getFormat()
+    {
+        return $this->format;
+    }
+
+
+    public function setEncoding($encoding)
+    {
+        $this->encoding = $encoding;
+        return $this;
+    }
+
+
+    public function getEncoding()
+    {
+        return $this->encoding;
+    }
 }
