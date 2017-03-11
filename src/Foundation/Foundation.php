bf41d8e49fccedaa6564b9fb0fcfa27688bb83db
Macc Liu <maccliu@qq.com>
2017-03-11 13:30:01 +0800

    dev Foundation class

--- src/Foundation/Foundation.php
+++ src/Foundation/Foundation.php
@@ -11,4 +11,14 @@ namespace Dida;
  */
 class Foundation
 {
+    private function handleErrorsAndExceptions()
+    {
+        if (DIDA_DEBUG_MODE) {
+            // 报告所有 PHP 错误，参见 PHP 的 error_reporting() 文档
+            error_reporting(E_ALL);
+        } else {
+            // 关闭所有 PHP 错误报告
+            error_reporting(0);
+        }
+    }
 }
