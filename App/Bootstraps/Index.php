d011932d891b3fc029927c990bbc6fb504fa7670
Macc Liu <maccliu@qq.com>
2017-03-12 16:09:02 +0800

    dev

--- App/Bootstraps/Index.php
+++ App/Bootstraps/Index.php
@@ -2,29 +2,27 @@
 
 namespace App;
 
-app()->singleton('request', function () {
+app()->set('request', function () {
     return new \Dida\RequestHttp();
 });
 
-app()->singleton('dispatcher', function () {
+app()->set('dispatcher', function () {
     return new \Dida\Dispatcher();
 });
 
-app()->singleton('router', function () {
+app()->set('router', function () {
     /* 生成 Router实例 */
     $router = new \Dida\Router();
 
-    /* 定义若干路由规则 */
-
-    // 短url路由
+    /* 定义短url路由规则 */
     $route1 = new \App\Routes\ShortUrlRoute;
     $router->addRoute($route1);
 
-    // 默认路由
+    /* 定义默认路由规则 */
     $route2 = new \App\Routes\DefaultRoute;
     $route2->loadRouteMap(DIDA_APP_ROOT . 'Config/DefaultRouteMap.php');
     $router->addRoute($route2);
 
-    //返回生成的路由器实例
+    /* 返回生成的路由器实例 */
     return $router;
 });
