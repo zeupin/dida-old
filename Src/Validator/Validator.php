0b8568184cfcc3157b69406e0d7d7e5d6c90fe59
Macc Liu <maccliu@qq.com>
2017-03-15 23:29:14 +0800

    新增Validator类

---
+++ Src/Validator/Validator.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Dida Framework --Powered by Zeupin LLC
+ * http://dida.zeupin.com
+ */
+
+namespace Dida\Validator;
+
+/**
+ * Validator 基类
+ */
+abstract class Validator implements ValidatorInterface
+{
+}
---
+++ Src/Validator/ValidatorInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Dida Framework --Powered by Zeupin LLC
+ * http://dida.zeupin.com
+ */
+
+namespace Dida\Validator;
+
+/**
+ * Validator 验证器标准接口
+ */
+interface ValidatorInterface
+{
+    public function validate();
+}
