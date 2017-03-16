c4a12ba5d995b3167dba202d55249e99dd9400a5
Macc Liu <maccliu@qq.com>
2017-03-16 19:37:31 +0800

    dev

--- Src/Validator/Validator.php
+++ Src/Validator/Validator.php
@@ -11,5 +11,29 @@ namespace Dida\Validator;
  */
 abstract class Validator
 {
+    protected $data;         // 待验证的数据
+    protected $errors = [];  // 显示验证的错误信息
+
+
+    /**
+     * 设置待验证的数据
+     */
+    abstract public function setData($data);
+
+
+    /**
+     * 对待数据进行验证
+     *
+     * @return bool 验证成功返回true，失败返回false
+     */
     abstract public function validate();
+
+
+    /**
+     * 验证失败时，获取错误信息
+     */
+    public function getErrors()
+    {
+        return $this->errors;
+    }
 }
