dfaf448fdc5d833d86f6279213a26febf14c26e3
Macc Liu <maccliu@qq.com>
2017-03-16 14:07:17 +0800

    Event

--- Src/Event/Event.php
+++ Src/Event/Event.php
@@ -11,4 +11,23 @@ namespace Dida;
  */
 abstract class Event
 {
+    protected $events = [];
+
+
+    /**
+     * 新增一个事件到一个baseEvent事件之前
+     */
+    public function addEventBefore($newEvent, $baseEvent);
+
+
+    /**
+     * 新增一个事件到一个baseEvent事件之后
+     */
+    public function addEventAfter($newEvent, $baseEvent);
+
+
+    /**
+     * 挂接一个处理程序到指定的事件上
+     */
+    public function hook($event, $handler, $parameters);
 }
