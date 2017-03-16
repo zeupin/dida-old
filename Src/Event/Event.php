2d63e4b12d576d7e33e3e8e70673e31f4e19e444
Macc Liu <maccliu@qq.com>
2017-03-16 20:47:19 +0800

    Event

--- Src/Event/Event.php
+++ Src/Event/Event.php
@@ -11,23 +11,115 @@ namespace Dida;
  */
 abstract class Event
 {
-    protected $events = [];
+    // 预定义的系统事件
+    protected $events = [
+        'dida_ready'      => 0,
+        'before_request'  => 0,
+        'after_request'   => 0,
+        'before_route'    => 0,
+        'after_route'     => 0,
+        'before_action'   => 0,
+        'after_action'    => 0,
+        'before_response' => 0,
+        'after_response'  => 0,
+    ];
+    protected $hooks = [];
 
 
     /**
-     * 新增一个事件到一个baseEvent事件之前
+     * 新增一个用户事件
      */
-    public function addEventBefore($newEvent, $baseEvent);
+    public function addEvent($event)
+    {
+        if (!array_key_exists($event, $this->events)) {
+            $this->events[$event] = 1;
+        }
+    }
 
 
     /**
-     * 新增一个事件到一个baseEvent事件之后
+     * 删除一个用户事件，以及所有已挂接到这个事件hooks。
+     * 注，预定义的系统事件是不可删除的。
      */
-    public function addEventAfter($newEvent, $baseEvent);
+    public function removeEvent($event)
+    {
+        if (array_key_exists($event, $this->events)) {
+            if ($this->events[$event] > 0) {
+                unset($this->events[$event]);
+                unset($this->hooks[$event]);
+            }
+        }
+    }
 
 
     /**
-     * 挂接一个处理程序到指定的事件上
+     * 检查一个事件是否已经定义
      */
-    public function hook($event, $handler, $parameters);
+    public function hasEvent($event)
+    {
+        return array_key_exists($event, $this->events);
+    }
+
+
+    /**
+     * 挂接一个处理程序到指定的event上
+     *
+     * @param string $event
+     * @param string $handler
+     * @param array $parameters
+     * @param string $id
+     */
+    public function hook($event, $handler, array $parameters = [], $id = DIDA_NOT_SET)
+    {
+        if ($this->hasEvent($event)) {
+            if ($id === DIDA_NOT_SET) {
+                $this->hooks[$event][] = [$handler, $parameters];
+            } else {
+                $this->hooks[$event][$id] = [$handler, $parameters];
+            }
+        } else {
+            throw new \Dida\Exception\EventNotFound();
+        }
+    }
+
+
+    /**
+     * 解除event上某个id的处理程序的挂接
+     *
+     * @param string $event
+     * @param string $id
+     */
+    public function unhook($event, $id)
+    {
+        unset($this->hooks[$event][$id]);
+    }
+
+
+    /**
+     * 清除挂接在某个event上的所有处理程序
+     *
+     * @param string $event
+     */
+    public function unhookAll($event)
+    {
+        unset($this->hooks[$event]);
+    }
+
+
+    /**
+     * 触发一个事件
+     */
+    public function tigger($event)
+    {
+        if (array_key_exists($event, $events)) {
+            if (array_key_exists($event, $this->hooks)) {
+                foreach ($hooks as $hook) {
+                    list($handler, $parameters) = $hook;
+                    call_user_func_array($handler, $parameters);
+                }
+            }
+        } else {
+            throw new \Dida\Exception\EventNotFound();
+        }
+    }
 }
