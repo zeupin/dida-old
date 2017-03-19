c27d80142ffffdf48af96cb0b64357e17a6289be
Macc Liu <maccliu@qq.com>
2017-03-19 22:32:57 +0800

    Db

---
+++ Src/Db/DMLTrait.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Dida Framework --Powered by Zeupin LLC
+ * http://dida.zeupin.com
+ */
+
+namespace Dida\Db;
+
+/**
+ * 数据操纵子语言（Data Manipulation Language，DML）
+ */
+trait DDLTrait
+{
+}
