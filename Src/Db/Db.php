fe400b1429956ca5576f9209a9d2c55e3c6fb6bd
Macc Liu <maccliu@qq.com>
2017-03-19 21:57:42 +0800

    Db

--- Src/Db/Db.php
+++ Src/Db/Db.php
@@ -11,6 +11,9 @@ namespace Dida;
  */
 abstract class Db implements DbInterface
 {
+    public $tablePrefix = '';
+
+
     /**
      * 读取数据库
      */
@@ -24,5 +27,19 @@ abstract class Db implements DbInterface
      */
     public function write()
     {
+    }
+
+
+    /**
+     * 把一个形式SQL语句转为真实SQL语句
+     *
+     * 把 ###table 替换为 prefix_table
+     * todo: ##field 替换为 field
+     *
+     * @param string $sql
+     */
+    public function realSql($sql)
+    {
+        return str_replace('###', $this->tablePrefix, $sql);
     }
 }
