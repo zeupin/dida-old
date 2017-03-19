0e4601eaaf3649ea7b1135a92c553085174a0d42
Macc Liu <maccliu@qq.com>
2017-03-19 23:46:27 +0800

    dev

--- Src/Session/Session.php
+++ Src/Session/Session.php
@@ -9,6 +9,28 @@ namespace Dida;
 /**
  * Session 类
  */
-abstract class Session implements \ArrayAccess
+class Session implements \ArrayAccess
 {
+    public function offsetExists($key)
+    {
+        return array_key_exists($key, $_SESSION);
+    }
+
+
+    public function offsetGet($key)
+    {
+        return ($this->offsetExists($key)) ? $_SESSION[$key] : DIDA_NOT_SET;
+    }
+
+
+    public function offsetSet($key, $value)
+    {
+        $_SESSION[$key] = $value;
+    }
+
+
+    public function offsetUnset($key)
+    {
+        unset($_SESSION[$key]);
+    }
 }
