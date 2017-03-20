badd8fbf458827ecbb9c3c5926fead3e61a71187
Macc Liu <maccliu@qq.com>
2017-03-20 22:02:29 +0800

    dev

--- www/index.php
+++ www/index.php
@@ -6,7 +6,7 @@
 /* 必填的常量 */
 define('DIDA_ROOT', realpath(__DIR__ . '/../src') . '/');
 define('DIDA_COMPOSER_ROOT', realpath(__DIR__ . '/../vendor') . '/');
-define('DIDA_APP_ROOT', realpath(__DIR__ . '/../App') . '/');
+define('DIDA_APP_ROOT', realpath(__DIR__ . '/../Builder') . '/');
 define('DIDA_VAR_ROOT', realpath(__DIR__ . '/../Var') . '/');
 define('DIDA_WEB_ROOT', __DIR__ . '/');
 define('DIDA_ENVIRON', 'dev');
@@ -14,7 +14,7 @@ define('DIDA_ENVIRON', 'dev');
 /* 选填的常量 */
 define('DIDA_WWW', '/');
 define('DIDA_DEBUG', true);
-define('DIDA_APP_NAMESPACE', 'App');
+define('DIDA_APP_NAMESPACE', 'Dida\\Builder');
 define('DIDA_DEFAULT_CONTROLLER', 'Index');
 define('DIDA_DEFAULT_ACTION', 'index');
 
