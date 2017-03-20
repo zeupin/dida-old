898cd3a06e851c9c1c72b5388795fa43fd1613a5
Macc Liu <maccliu@qq.com>
2017-03-20 17:46:03 +0800

    dev

--- Builder/Bootstraps/Index.php
+++ Builder/Bootstraps/Index.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace Dida\Builder;
 
 app()->set('request', function () {
     return new \Dida\RequestHttp();
@@ -13,10 +13,6 @@ app()->set('dispatcher', function () {
 app()->set('router', function () {
     /* 生成 Router实例 */
     $router = new \Dida\Router();
-
-    /* 定义短url路由规则 */
-    $route1 = new \App\Routes\ShortUrlRoute;
-    $router->addRoute($route1);
 
     /* 定义默认路由规则 */
     $route2 = new \Dida\Route\DefaultRoute;
