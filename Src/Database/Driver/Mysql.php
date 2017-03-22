617b168af53018011f670fb10eb3f33c02872867
Macc Liu <maccliu@qq.com>
2017-03-22 13:56:08 +0800

    Database/Driver 优化

--- Src/Database/Driver/Mysql.php
+++ Src/Database/Driver/Mysql.php
@@ -11,7 +11,19 @@ namespace Dida\Database\Driver;
  */
 class Mysql extends Driver
 {
+    public function __construct(array $config)
+    {
+        if (isset($config['socket'])) {
+            $this->socket($config);
+        } else {
+            $this->hostport($config);
+        }
+    }
+
+
     /**
+     * host-port模式
+     *
      * 必填:
      *     user, password, dbname
      * 选填:
@@ -19,7 +31,7 @@ class Mysql extends Driver
      *     port    默认 3306
      *     charset 默认 utf8
      */
-    public function __construct(array $config)
+    private function hostport($config)
     {
         /* 必填参数 */
         if (!isset($config['user']) ||
@@ -43,6 +55,44 @@ class Mysql extends Driver
         ];
         $this->_user = $user;
         $this->_password = $password;
+        $this->_dbname = $dbname;
+        $this->_charset = $charset;
+    }
+
+
+    /**
+     * socket模式
+     *
+     * 必填:
+     *     socket, user, password, dbname
+     * 选填:
+     *     charset 默认 utf8
+     */
+    private function socket(array $config)
+    {
+        /* 必填参数 */
+        if (!isset($config['socket']) ||
+            !isset($config['user']) ||
+            !isset($config['password']) ||
+            !isset($config['dbname'])) {
+            throw new \Dida\DatabaseConnectionException();
+            return false;
+        }
+        $socket = $config['socket'];
+        $user = $config['user'];
+        $password = $config['password'];
+        $dbname = $config['dbname'];
+
+        /* 选填参数 */
+        $charset = isset($config['charset']) ? $config['charset'] : 'utf8';
+
+        /* 设置 */
+        $this->_dsn = sprintf('mysql:unix_socket=%s;dbname=%s;charset=%s', $socket, $dbname, $charset);
+        $this->_user = $user;
+        $this->_password = $password;
+        $this->_options = [
+            \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES ' . $charset,
+        ];
         $this->_dbname = $dbname;
         $this->_charset = $charset;
     }
--- Src/Database/Driver/Sqlsrv.php
+++ Src/Database/Driver/Sqlsrv.php
@@ -7,17 +7,29 @@
 namespace Dida\Database\Driver;
 
 /**
- * MS SQL Server驱动 (Windows环境)
+ * MS SQL Server驱动
  */
-class Sqlsrv extends Driver
+class Sqlserver extends Driver
 {
+    public function __construct(array $config)
+    {
+        if (isset($config['server'])) {
+            $this->sqlsrv($config);
+        } else {
+            $this->dblib($config);
+        }
+    }
+
+
     /**
+     * sqlsrv模式 (Windows环境)
+     *
      * 必填:
      *     server, user, password, dbname
      * 选填:
      *     charset 默认 utf8
      */
-    public function __construct(array $config)
+    private function sqlsrv(array $config)
     {
         /* 必填参数 */
         if (!isset($config['server']) ||
@@ -37,6 +49,44 @@ class Sqlsrv extends Driver
 
         /* 设置 */
         $this->_dsn = sprintf('sqlsrv:server=%s;database=%s;', $server, $dbname);
+        $this->_user = $user;
+        $this->_password = $password;
+        $this->_options = [];
+        $this->_dbname = $dbname;
+        $this->_charset = $charset;
+    }
+
+
+    /**
+     * dblib模式 (Linux环境)
+     *
+     * 必填:
+     *     host, port, user, password, dbname
+     * 选填:
+     *     charset 默认 utf8
+     */
+    private function dblib(array $config)
+    {
+        /* 必填参数 */
+        if (!isset($config['host']) ||
+            !isset($config['port']) ||
+            !isset($config['user']) ||
+            !isset($config['password']) ||
+            !isset($config['dbname'])) {
+            throw new \Dida\DatabaseConnectionException();
+            return false;
+        }
+        $host = $config['host'];
+        $port = $config['port'];
+        $user = $config['user'];
+        $password = $config['password'];
+        $dbname = $config['dbname'];
+
+        /* 选填参数 */
+        $charset = isset($config['charset']) ? $config['charset'] : 'utf8';
+
+        /* 设置 */
+        $this->_dsn = sprintf('dblib:host=%s:%s;dbname=%s;charset=%s', $host, $port, $dbname, $charset);
         $this->_user = $user;
         $this->_password = $password;
         $this->_options = [];
