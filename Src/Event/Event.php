f0617b9aaea8f2c2926c5061e0c45caccd28c197
Macc Liu <maccliu@qq.com>
2017-03-29 23:16:56 +0800

    修改 Event

--- Src/Event/Event.php
+++ Src/Event/Event.php
@@ -11,17 +11,20 @@ namespace Dida;
  */
 abstract class Event
 {
+    const SYSTEM_EVENT = 0;
+    const USER_EVENT = 1;
+
     // 预定义的系统事件
     protected $events = [
-        'dida_ready'      => 0,
-        'before_request'  => 0,
-        'after_request'   => 0,
-        'before_route'    => 0,
-        'after_route'     => 0,
-        'before_action'   => 0,
-        'after_action'    => 0,
-        'before_response' => 0,
-        'after_response'  => 0,
+        'dida_ready'      => self::SYSTEM_EVENT,
+        'before_request'  => self::SYSTEM_EVENT,
+        'after_request'   => self::SYSTEM_EVENT,
+        'before_route'    => self::SYSTEM_EVENT,
+        'after_route'     => self::SYSTEM_EVENT,
+        'before_action'   => self::SYSTEM_EVENT,
+        'after_action'    => self::SYSTEM_EVENT,
+        'before_response' => self::SYSTEM_EVENT,
+        'after_response'  => self::SYSTEM_EVENT,
     ];
     protected $hooks = [];
 
@@ -32,23 +35,25 @@ abstract class Event
     public function addEvent($event)
     {
         if (!array_key_exists($event, $this->events)) {
-            $this->events[$event] = 1;
+            $this->events[$event] = self::USER_EVENT;
         }
+        return $this;
     }
 
 
     /**
      * 删除一个用户事件，以及所有已挂接到这个事件hooks。
-     * 注，预定义的系统事件是不可删除的。
+     * 注：系统事件不可删除。
      */
     public function removeEvent($event)
     {
         if (array_key_exists($event, $this->events)) {
-            if ($this->events[$event] > 0) {
+            if ($this->events[$event] == self::USER_EVENT) {
                 unset($this->events[$event]);
                 unset($this->hooks[$event]);
             }
         }
+        return $this;
     }
 
 
@@ -80,6 +85,7 @@ abstract class Event
         } else {
             throw new \Dida\Exception\EventNotFound();
         }
+        return $this;
     }
 
 
@@ -92,11 +98,12 @@ abstract class Event
     public function unhook($event, $id)
     {
         unset($this->hooks[$event][$id]);
+        return $this;
     }
 
 
     /**
-     * 清除挂接在某个event上的所有处理程序
+     * 清除挂接在指定event上的所有处理程序
      *
      * @param string $event
      */
@@ -111,15 +118,24 @@ abstract class Event
      */
     public function tigger($event)
     {
-        if (array_key_exists($event, $events)) {
-            if (array_key_exists($event, $this->hooks)) {
-                foreach ($hooks as $hook) {
-                    list($handler, $parameters) = $hook;
-                    call_user_func_array($handler, $parameters);
+        if (array_key_exists($event, $this->hooks)) {
+            /*
+             * 依次执行处理程序
+             * 注：如果某个处理程序返回false，则不再执行后面的处理程序。
+             */
+            foreach ($hooks as $hook) {
+                list($handler, $parameters) = $hook;
+                if (call_user_func_array($handler, $parameters) === false) {
+                    break;
                 }
             }
         } else {
-            throw new \Dida\Exception\EventNotFound();
+            /*
+             * 如果事件不存在，抛出EventNotFound异常
+             */
+            if (!array_key_exists($event, $events)) {
+                throw new \Dida\Exception\EventNotFound();
+            }
         }
     }
 }
