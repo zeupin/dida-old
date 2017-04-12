66cb64869280985d54d94e5d7c5929f1063ff74f
Macc Liu <maccliu@qq.com>
2017-04-12 18:41:06 +0800

    新增 ValueNotSetException

---
+++ Src/Exception/ValueNotSetException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Dida Framework --Powered by Zeupin LLC
+ * http://dida.zeupin.com
+ */
+
+namespace Dida\Exception;
+
+use \Dida\Exception;
+
+/**
+ * ValueNotSetException 变量/属性虽然存在，但是值尚未设置
+ */
+class ValueNotSetException extends Exception
+{
+}
