1f2f744fbda358d5d98fecac2ce9f6f08d193b3a
Macc Liu <maccliu@qq.com>
2017-04-13 23:19:06 +0800

    新增 Debug 的 html模板页面

---
+++ Src/Debug/View/Html.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="zh">
+<head>
+  <meta charset="UTF-8">
+  <title>Debug</title>
+  <style>
+    * {font-family:'新宋体','Courier New','Monospace','Kai';}
+  </style>
+</head>
+<body>
+  <h1>Debug</h1>
+  <?= $content ?>
+</body>
+</html>
