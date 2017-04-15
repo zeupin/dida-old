37afb1ba5f5eece028bfde9de4522bee631f6aa2
Macc Liu <maccliu@qq.com>
2017-04-15 23:19:59 +0800

    修改 Response

--- Src/Response/Response.php
+++ Src/Response/Response.php
@@ -13,21 +13,25 @@ class Response
 {
     public $contentType = 'html';       // html,json,text
     public $encoding = 'utf-8';
-    public $buffer = [];
+    public $data = [];
 
 
     public function clear()
     {
-        $this->buffer = [];
+        $this->data = [];
     }
 
 
-    public function sendHeader($header, $replace = false, $http_response_code = 0)
+    public function addData($data)
     {
-        if ($http_response_code > 99) {
-            header($header, $replace, $http_response_code);
-        } else {
-            header($header, $replace);
+        $this->data[] = $data;
+    }
+
+
+    public function output()
+    {
+        if ($this->contentType == 'json') {
+            return json_encode($this->data);
         }
     }
 
@@ -38,16 +42,26 @@ class Response
     }
 
 
-    public function addData($data)
+    /**
+     * 检查是否是一个有效的HTTP状态码
+     *
+     * 一个有效的 HTTP 状态码是一个三位数，由 RFC 2616 规范定义的，参阅百度百科“HTTP状态码”词条。
+     * 1xx  消息
+     * 2xx  成功
+     * 3xx  重定向
+     * 4xx  请求错误
+     * 5xx  服务器错误
+     * 6xx  其它错误
+     *
+     * @param int $http_status_code
+     * @return boolean
+     */
+    public function isValidHttpStatusCode($http_status_code)
     {
-        $this->buffer[] = $data;
-    }
-
-
-    public function output()
-    {
-        if ($this->contentType == 'json') {
-            return json_encode($this->buffer);
+        if (is_int($http_status_code) && $http_status_code > 99 && $http_status_code < 700) {
+            return true;
+        } else {
+            return false;
         }
     }
 }
