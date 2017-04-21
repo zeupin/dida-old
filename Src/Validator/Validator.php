c1ac72aa468be97a85928678c5888e97fec5b0da
Macc Liu <maccliu@qq.com>
2017-04-21 19:58:20 +0800

    Validator 新增构造函数

--- Src/Validator/Validator.php
+++ Src/Validator/Validator.php
@@ -11,16 +11,25 @@ namespace Dida\Validator;
  */
 abstract class Validator
 {
-    protected $data;         // 待验证的数据
-    protected $errors = [];  // 显示验证的错误信息
+    protected $parameters = [];     // 验证时需要的参数
+    protected $data;                // 待验证的数据
+    protected $errors = [];         // 显示验证的错误信息
 
-    
     /**
-     * 抽象方法，对待数据进行验证
+     * 抽象方法：对数据进行验证
      *
      * @return bool 验证成功返回true，失败返回false
      */
     abstract public function validate();
+
+
+    /**
+     * 抽象方法：构造函数
+     */
+    public function __construct($parameters = [])
+    {
+        $this->parameters = $parameters;
+    }
 
 
     /**
