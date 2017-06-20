1571767f13c9e95226eeeddf62110c8f1a551bbe
Macc Liu <maccliu@qq.com>
2017-06-20 11:34:30 +0800

    简化路径常量设置，避免因为客户没有遵守约定，没有在目录常量的末尾加上 DIRECTORY_SEPARATOR 而导致程序出现问题

--- Src/Constants.php
+++ Src/Constants.php
@@ -3,28 +3,68 @@
  * Dida Framework --Powered by Zeupin LLC
  * http://dida.zeupin.com
  */
-/* 是否开启调试模式 */
-defined('DIDA_DEBUG') || define('DIDA_DEBUG', true); // 默认开启调试模式
 
-/* APP的名称空间 */
-defined('DIDA_APP_NAMESPACE') || define('DIDA_APP_NAMESPACE', 'App'); // 默认值为App
 
-/* 是否是CLI模式 */
-define('DIDA_IS_CLI', PHP_SAPI === 'cli');
+/**
+ * 必填的常量
+ */
 
-/* 指示这是未设置的函数参数 */
-define('DIDA_NOT_SET', "\0");
+// 路径常量
+$dida_system_constants = [
+    'DIDA_ROOT',
+    'DIDA_COMPOSER_ROOT',
+    'DIDA_APP_ROOT',
+    'DIDA_VAR_ROOT',
+    'DIDA_WEB_ROOT',
+];
 
-/* 默认的Controller和Action */
-defined('DIDA_DEFAULT_CONTROLLER') || define('DIDA_DEFAULT_CONTROLLER', 'Index'); // DefaultController默认值为Index
-defined('DIDA_DEFAULT_ACTION') || define('DIDA_DEFAULT_ACTION', 'index'); // DefaultAction默认值为index
-
-/* 如果不是CLI模式，则为HTTP模式 */
-if (!DIDA_IS_CLI) {
-    /* 如果没有定义 DIDA_WWW，则默认其为当前脚本所在目录 */
-    if (!defined('DIDA_WWW')) {
-        $dida_www = str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME']));
-        define('DIDA_WWW', ($dida_www === '/') ? '/' : $dida_www . '/');
-        unset($dida_www);
+foreach ($dida_system_constants as $const) {
+    if (!isset($$const)) {
+        die("The necessary configure variable ${$const} is not set.");
+    } elseif (!file_exists($$const) || !is_dir($$const)) {
+        die("${$const}" . $$const . ", but this directory does not exist.");
+    } else {
+        $path = realpath($$const) . DIRECTORY_SEPARATOR;
+        define($const, $path);
+        unset($$const);
     }
 }
+
+// DIDA_ENV
+if (!isset($DIDA_ENV)) {
+    die('The necessary configure variable $DIDA_ENV is not set.');
+} else {
+    define('DIDA_ENV', $DIDA_ENV);
+    unset($DIDA_ENV);
+}
+
+
+/**
+ * 选填的常量
+ */
+
+// DIDA_WWW
+define('DIDA_WWW', isset($DIDA_WWW) ? $DIDA_WWW : '/');
+
+// DIDA_DEBUG 是否开启调试模式
+define('DIDA_DEBUG', isset($DIDA_DEBUG) ? $DIDA_DEBUG : false);
+
+// DIDA_APP_NAMESPACE APP的名称空间
+define('DIDA_APP_NAMESPACE', isset($DIDA_APP_NAMESPACE) ? $DIDA_APP_NAMESPACE : 'App');
+
+// DIDA_DEFAULT_CONTROLLER
+define('DIDA_DEFAULT_CONTROLLER', isset($DIDA_DEFAULT_CONTROLLER) ? $DIDA_DEFAULT_CONTROLLER : 'Index');
+
+// DIDA_DEFAULT_ACTION
+define('DIDA_DEFAULT_ACTION', isset($DIDA_DEFAULT_ACTION) ? $DIDA_DEFAULT_ACTION : 'index');
+
+
+/**
+ * 内置的常量
+ */
+
+// DIDA_IS_CLI 是否是CLI模式
+define('DIDA_IS_CLI', PHP_SAPI === 'cli');
+
+// DIDA_NOT_SET 指示这是未设置的函数参数
+define('DIDA_NOT_SET', "\0");
--- www/index.php
+++ www/index.php
@@ -3,20 +3,21 @@
  * Dida Framework --Powered by Zeupin LLC
  * http://dida.zeupin.com
  */
+ 
 /* 必填的常量 */
-define('DIDA_ROOT',           __DIR__ . '/../src/');
-define('DIDA_COMPOSER_ROOT',  __DIR__ . '/../vendor/');
-define('DIDA_APP_ROOT',       __DIR__ . '/../App/');
-define('DIDA_VAR_ROOT',       __DIR__ . '/../Var/');
-define('DIDA_WEB_ROOT',       __DIR__ . '/');
-define('DIDA_ENV',            'dev');
+$DIDA_ROOT =           __DIR__ . '/../src/';
+$DIDA_COMPOSER_ROOT =  __DIR__ . '/../vendor/';
+$DIDA_APP_ROOT =       __DIR__ . '/../App/';
+$DIDA_VAR_ROOT =       __DIR__ . '/../Var/';
+$DIDA_WEB_ROOT =       __DIR__ . '/';
+$DIDA_ENV =            'dev';
 
 /* 选填的常量 */
-define('DIDA_WWW', '/');
-define('DIDA_DEBUG', true);
-define('DIDA_APP_NAMESPACE', 'Dida\\Builder');
-define('DIDA_DEFAULT_CONTROLLER', 'Index');
-define('DIDA_DEFAULT_ACTION', 'index');
+$DIDA_WWW = '/';
+$DIDA_DEBUG = true;
+$DIDA_APP_NAMESPACE = 'Dida\\Builder';
+$DIDA_DEFAULT_CONTROLLER = 'Index';
+$DIDA_DEFAULT_ACTION = 'index';
 
 /* 开始 */
-require(DIDA_ROOT . 'Index.php');
+require($DIDA_ROOT . 'Index.php');
