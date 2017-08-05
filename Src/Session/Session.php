9df1d1e9023e3020c4c14eb2529b9436792b57fc
Macc Liu <maccliu@qq.com>
2017-08-05 13:10:56 +0800

    Session

--- Src/Session/Session.php
+++ Src/Session/Session.php
@@ -11,6 +11,8 @@ namespace Dida;
  */
 class Session implements \ArrayAccess
 {
+
+
     public function offsetExists($key)
     {
         return array_key_exists($key, $_SESSION);
@@ -32,5 +34,49 @@ class Session implements \ArrayAccess
     public function offsetUnset($key)
     {
         unset($_SESSION[$key]);
+    }
+
+
+    public function set($key, $value)
+    {
+        $_SESSION[$key] = $value;
+    }
+
+
+    public function get($key)
+    {
+        return $_SESSION[$key];
+    }
+
+
+    public function has($key)
+    {
+        return array_key_exists($key, $_SESSION);
+    }
+
+
+    public function remove($key)
+    {
+        unset($_SESSION[$key]);
+    }
+
+
+    /**
+     * 删除所有$_SESSION变量
+     *
+     * @param array $except[] 可以指定保留哪些SESSION变量
+     */
+    public function clear($except = [])
+    {
+        $session = [];
+        foreach ($except as $key) {
+            if (array_key_exists($key, $_SESSION)) {
+                $session[$key] = $_SESSION;
+            }
+        }
+        session_unset();
+        foreach ($session as $key => $value) {
+            $_SESSION[$key] = $value;
+        }
     }
 }
